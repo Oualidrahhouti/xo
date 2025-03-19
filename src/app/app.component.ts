6d57dc775143988d154a6b46569e3ed389cb6884
oualidrahhouti <rahhoutiwalid@gmail.com>
2025-03-19 23:06:32 +0100

    Implementing the overall game logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Square } from './Component/square/square.component';
+import { GameService, Player } from './Service/game.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Square],
+  imports: [RouterOutlet, Square, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'xo';
+  currentPlayer: Player = 'X';
+  winner: Player | null = null;
+  gameOver: boolean = false;
+  squares = Array(9).fill(null);
+
+  constructor(private gameSerivce: GameService) {
+    this.gameSerivce.currentPlayer$.subscribe((player) => {
+      this.currentPlayer = player;
+    });
+
+    this.gameSerivce.winner$.subscribe((winner) => {
+      this.winner = winner;
+    });
+
+    this.gameSerivce.gameOver$.subscribe((gameOver) => {
+      this.gameOver = gameOver;
+    });
+  }
+
+  resetGame(): void {
+    this.gameSerivce.resetGame();
+  }
 }
